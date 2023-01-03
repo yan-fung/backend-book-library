bf24d5295aec7b86b0f169f79d300a1cdc1c2893
Yan Fung <fungmingyan@hotmail.com>
2023-01-03 20:48:47 +0000

    refactored some code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const APP_PORT = process.env.PORT || 4000;
+const APP_PORT = 4000;
 
 app.listen(APP_PORT, () => {
     console.log(`App is listening on port ${APP_PORT}`)
