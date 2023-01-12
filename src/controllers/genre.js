c39e9dcce95e7c5e35acc50f0ac0116cef809f64
Yan Fung <fungmingyan@hotmail.com>
2023-01-12 22:47:45 +0000

    Completed CRUD endpoints and created controller functions

---
+++ src/controllers/genre.js
@@ -0,0 +1,28 @@
+const {
+    createItem,
+    getAllItems,
+    getItemById,
+    updateItem,
+    deleteItem
+} = require('./helpers')
+
+
+const createGenre = (req, res) => createItem(res, 'genre', req.body);
+
+const getAllGenre = (_req, res) => getAllItems(res, 'genre');
+
+const getGenreById = (req, res) => getItemById(res, 'genre', req.params.id);
+
+const updateGenreById = (req, res) => updateItem(res, 'genre', req.body, req.params.id);
+
+const deleteGenreById = (req, res) => deleteItem(res, 'genre', req.params.id);
+
+
+
+module.exports = {
+    createGenre,
+    getAllGenre,
+    getGenreById,
+    updateGenreById,
+    deleteGenreById
+}
---
+++ src/routes/genre.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const genreRouter = express.Router();
+
+const genreController = require('../controllers/genre');
+
+
+genreRouter.post('/', genreController.createGenre);
+genreRouter.get('/', genreController.getAllGenre);
+genreRouter.get('/:id', genreController.getGenreById);
+genreRouter.patch('/:id', genreController.updateGenreById);
+genreRouter.delete('/:id', genreController.deleteGenreById);
+
+
+
+
+module.exports = genreRouter
