8b0cd6c748440dad8471ac2121963b4202872988
Yan Fung <fungmingyan@hotmail.com>
2023-01-12 22:47:03 +0000

    Completed CRUD endpoints and created controller functions

---
+++ src/controllers/author.js
@@ -0,0 +1,26 @@
+const {
+    createItem,
+    getAllItems,
+    getItemById,
+    updateItem,
+    deleteItem
+} = require('./helpers');
+
+
+const createAuthor = (req, res) => createItem(res, 'author', req.body);
+
+const getAllAuthor = (_req, res) => getAllItems(res, 'author');
+
+const getAuthorById = (req, res) => getItemById(res, 'author', req.params.id);
+
+const updateAuthorById = (req, res) => updateItem(res, 'author', req.body, req.params.id);
+
+const deleteAuthorById = (req, res) => deleteItem(res, 'author', req.params.id);
+
+module.exports = {
+    createAuthor,
+    getAllAuthor,
+    getAuthorById,
+    updateAuthorById,
+    deleteAuthorById
+};
---
+++ src/routes/author.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const authorRouter = express.Router();
+
+const authorController = require('../controllers/author');
+
+
+authorRouter.post('/', authorController.createAuthor);
+authorRouter.get('/', authorController.getAllAuthor);
+authorRouter.get('/:id', authorController.getAuthorById);
+authorRouter.patch('/:id', authorController.updateAuthorById);
+authorRouter.delete('/:id', authorController.deleteAuthorById);
+
+module.exports = authorRouter
